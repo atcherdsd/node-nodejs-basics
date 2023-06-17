648be5b78bf8a3760ca1dd1e7771c65e75eb29af
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-06-17 14:25:09 +0300

    feat: add streams realisation

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = path.join(
+        path.dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToRead.txt'
+    );
+    const readStream = createReadStream(pathToFile);
+    let content = '';
+
+    readStream.on('data', chunk => {
+        content += chunk.toString();
+    });
+    readStream.on('end', () => process.stdout.write(content));   
+    readStream.on('error', err => console.error(err.message));
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    const readStream = process.stdin;
+    const writeStream = process.stdout;
+    
+    try {
+        const transformStream = new Transform({
+            transform(chunk, encoding, cb) {
+                const chunkStringified = chunk.toString().trim();
+                const reversedChunk = chunkStringified.split('').reverse().join('');
+                this.push(reversedChunk + '\n');
+                cb();
+            }
+        });
+        await pipeline(readStream, transformStream, writeStream);
+    } catch (err) {
+        throw Error(err.message);
+    }
+    
 };
 
 await transform();
