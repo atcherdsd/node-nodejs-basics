bc3dd348bf68f786be75c9d7b6f26f124700c8ea
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-06-17 08:19:42 +0300

    refactor: add trim() to args.js to proply handle the end of a line

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,13 +1,13 @@
 import { argv } from 'node:process';
 
 const parseArgs = () => {
-   
+
     const argsList = argv.slice(2)
-        .map((elem) => elem.startsWith('--') 
+        .map(elem => elem.startsWith('--') 
             ? elem = elem.slice(2) + ' is '
             : elem = elem + ', '
         )
-        .join('').slice(0, -2);
+        .join('').trim().slice(0, -1);
     
     console.log(argsList);
 };
